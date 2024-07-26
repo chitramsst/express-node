70c75be2fbc7b2bd4b9ba6e57f7b486721e18539
chitramsst <chitra.msst@gmail.com>
2024-07-26 10:15:31 +0530

    mongodb connection

---
+++ 03_vidtube/src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
+
+const ConnectDB = async() => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n Mongo connected! DB host : ${connectionInstance.connection.host}`)
+    } catch (e) {
+        console.log('MongoDB connection error',error)
+        process.exit(1)
+    }
+}
+
+export default ConnectDB;
--- 03_vidtube/src/index.js
+++ 03_vidtube/src/index.js
@@ -1,1 +1,20 @@
-console.log("haiu test 111")
+
+import {app} from './app.js'
+import dotenv from 'dotenv'
+import ConnectDB from './db/index.js'
+
+dotenv.config({
+path: './.env'
+})
+
+const PORT =  process.env.PORT || 4000
+
+ConnectDB()
+.then(() => {
+  app.listen(PORT, () => {
+    console.log(`listen to port: ${PORT} `);
+  })  
+})
+.catch((err)=>{
+  console.log("conneciton error")
+})
